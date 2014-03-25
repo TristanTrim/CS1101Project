6405ce093acf504905f72eda0906fc43ec63d392
herritt <randy.herritt@gmail.com>
2014-03-25 10:43:03 -0300

    You can now go to every room in the ship

--- Ship.java
+++ Ship.java
@@ -48,9 +48,14 @@ public class Ship
       
       //room descriptions
       String d_bridge = "";
-      String d_hallway = "";
       String d_readyRoom = "";
+      String d_hallway = "";
+      String d_holoDeck = "";
+      String d_tenForward = "";
+      String d_engineRoom = "";
+      String d_transporterRoom = "";
       String d_warRoom = "";
+      String d_crewCabins = "";
       
       d_bridge = "You are on the bridge of the USS Enterprise. People are <br>" +
           "walking around quickly, chattering to each other in hushed, <br>" +
@@ -62,15 +67,19 @@ public class Ship
           "called. You are also not entirely sure if Vulcans can look nervous<br>" +
           "or not, but he seems to be showing definite signs of concern. The<br>" +
           "Captain is here. He is sitting in his chair, looking distracted.";
-      d_hallway = "You are in a hallway.  You're still learning your way around<br>" +
-            "but you think this place looks familiar - in that, it looks like<br>" +
-            "every other hallway on the Enterprise. Sulu is here, shirtless, and<br>" +
-            "carrying a sword. But, other than that, things seem quite ordinary.";
       d_readyRoom = "You are in the Captain's Ready Room.  You are pretty<br>" +
            "sure you're not suppose to be in here. You can't get over how<br>" +
            "immaculate the room is. There doesn't seem to be single thing out<br>" +
            "of place. You see a desk, and a picture of a green alien woman<br>" +
            "drinking a Romulan ale.";
+      d_hallway = "You are in a hallway.  You're still learning your way around<br>" +
+            "but you think this place looks familiar - in that, it looks like<br>" +
+            "every other hallway on the Enterprise. Sulu is here, shirtless, and<br>" +
+            "carrying a sword. But, other than that, things seem quite ordinary.";
+      d_holoDeck = "You are in the holodeck.";
+      d_tenForward = "You are in Ten Forward.";
+      d_engineRoom = "You are in the engine room.";
+      d_transporterRoom = "You are in the transporter room.";          
       d_warRoom = "You are in the war room. You know for a fact that you're <br>" +
             "not supposed to be in here. Why do you keep doing this to<br>" +
             "yourself. Are you trying to get reprimanded or something. I mean<br>" +
@@ -81,7 +90,9 @@ public class Ship
             "you are in the war room. It is quite spectacular. You should <br>" +
             "see it. I'm not going to tell you about it though, because I<br>" +
             "I think you should know better, and if I do, I don't think you'll<br>" +
-            "ever learn.";       
+            "ever learn.";  
+      d_crewCabins = "You are in a section of the ship containing some of the<br>" +
+            "crew's cabins.";            
       
       //option text
       String bridgeOption3 = "";
@@ -101,42 +112,73 @@ public class Ship
          "are regretting not paying more attention to in class.";
        bridgeOption4 = "Kirk looks at you for a moment and then at your hands. <br>" +
          "\"You're... not... carrying my... tea.\" he says.  He looks <br>" +
-         "back to the floor, seeming to lose interest in you.";
-       
-      bridge.setDescription(d_bridge);
+         "back to the floor, seeming to lose interest in you.";     
       bridge.setOption1(new Option("North", d_hallway, 2));
       bridge.setOption2(new Option("East", d_readyRoom, 1));
       bridge.setOption3(new Option("Talk to Spock", bridgeOption3));
       bridge.setOption4(new Option("Talk to Kirk", bridgeOption4));
       bridge.setOption5(new Option("Look at viewscreen", "Looks spacy. Nice resolution"));
    
-      //hallway
-      hallway.setDescription(d_hallway);
-      hallway.setOption1(new Option("North", "ya go north"));
-      hallway.setOption2(new Option("South", d_bridge, 0));
-      hallway.setOption3(new Option("Wait", "You wait. It's kinda booring."));
-      hallway.setOption4(new Option("Do a handstand", "That wouldn't be very professional"));
-      hallway.setOption5(new Option("Pick mushroom", "These aren't mushrooms at all!<br>They are glass cups!", new Item("Glass"), null, ""));
-      
       //ready room
       readyRoomOption3 = "She seems quite lovely. There's some writing on the picture <br>" +
          "in purple lipstick in a language that you don't understand.";
       readyRoomOption4 = "You feel important, if not a little worried you might get<br>" +
          "caught. Although, you feel like you should be more concerned<br>" +
          "about the imminent attack of the Klingons.";
-
       captainReadyRoom.setOption1(new Option("West", d_bridge, 0));
       captainReadyRoom.setOption2(new Option("East", d_warRoom, 7));
       captainReadyRoom.setOption3(new Option("Look at picture", readyRoomOption3));
       captainReadyRoom.setOption4(new Option("Sit at desk", readyRoomOption4));
       captainReadyRoom.setOption5(new Option("Ponder being the Captain", "You seem to wasting a lot of time..."));
-   
+
+      //hallway
+      hallway.setOption1(new Option("North", d_holoDeck, 3));
+      hallway.setOption2(new Option("South", d_bridge, 0));
+      hallway.setOption3(new Option("East", d_crewCabins, 8));
+      hallway.setOption4(new Option("Do a handstand", "That wouldn't be very professional"));
+      hallway.setOption5(new Option("Pick mushroom", "These aren't mushrooms at all!<br>They are glass cups!", new Item("Glass"), null, ""));
+      
+      //holodeck
+      holoDeck.setOption1(new Option("East", d_tenForward, 4));
+      holoDeck.setOption2(new Option("South", d_hallway, 2));
+      holoDeck.setOption3(new Option("South", d_hallway, 2));
+      holoDeck.setOption4(new Option("South", d_hallway, 2));
+      holoDeck.setOption5(new Option("South", d_hallway, 2));
+      
+      //ten forward
+      tenForward.setOption1(new Option("West", d_holoDeck, 3));
+      tenForward.setOption2(new Option("East", d_engineRoom, 5));
+      tenForward.setOption3(new Option("South", d_crewCabins, 8));
+      tenForward.setOption4(new Option("South", d_crewCabins, 8));
+      tenForward.setOption5(new Option("South", d_crewCabins, 8));
+      
+      //engine room
+      engineRoom.setOption1(new Option("West", d_tenForward, 4));
+      engineRoom.setOption2(new Option("South", d_transporterRoom, 6));
+      engineRoom.setOption3(new Option("South", d_transporterRoom, 6));
+      engineRoom.setOption4(new Option("South", d_transporterRoom, 6));
+      engineRoom.setOption5(new Option("South", d_transporterRoom, 6));
+      
+      //transporter room
+      transporterRoom.setOption1(new Option("North", d_engineRoom, 5));
+      transporterRoom.setOption2(new Option("West", d_crewCabins, 8));
+      transporterRoom.setOption3(new Option("South", d_warRoom, 7));
+      transporterRoom.setOption4(new Option("South", d_warRoom, 7));
+      transporterRoom.setOption5(new Option("South", d_warRoom, 7));
+      
       //war room
-      warRoom.setOption1(new Option("West", d_readyRoom, 1));
+      warRoom.setOption1(new Option("North", d_transporterRoom, 6));
       warRoom.setOption2(new Option("West", d_readyRoom, 1));
       warRoom.setOption3(new Option("West", d_readyRoom, 1));
       warRoom.setOption4(new Option("West", d_readyRoom, 1));
       warRoom.setOption5(new Option("West", d_readyRoom, 1));
+      
+     //crew's cabins
+     crewCabins.setOption1(new Option("North", d_tenForward, 4));
+     crewCabins.setOption2(new Option("West", d_hallway, 2));
+     crewCabins.setOption3(new Option("East", d_transporterRoom, 6));
+     crewCabins.setOption4(new Option("East", d_transporterRoom, 6));
+     crewCabins.setOption5(new Option("East", d_transporterRoom, 6));
    
    }
 
